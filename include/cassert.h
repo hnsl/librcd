7c38f38621dab6c6f066f534b420db3f21dfc1ab
Simon Lindholm <simon.lindholm10@gmail.com>
2014-08-31 01:49:35 +0200

    cassert: add a unique counter, fix doc

--- include/cassert.h
+++ include/cassert.h
@@ -1,39 +1,30 @@
-/* CC-BY-SA 3.0 from https://stackoverflow.com/a/809465/68204 */
+/* CC-BY-SA 3.0, modified from https://stackoverflow.com/a/809465/68204 */
 /* See the COPYING file distributed with this project for more information. */
-
-/** A compile time assertion check.
- *
- *  Validate at compile time that the predicate is true without
- *  generating code. This can be used at any point in a source file
- *  where typedef is legal.
- *
- *  On success, compilation proceeds normally.
- *
- *  On failure, attempts to typedef an array type of negative size. The
- *  offending line will look like
- *      typedef assertion_failed_file_h_42[-1]
- *  where file is the content of the second parameter which should
- *  typically be related in some obvious way to the containing file
- *  name, 42 is the line number in the file on which the assertion
- *  appears, and -1 is the result of a calculation based on the
- *  predicate failing.
- *
- *  \param predicate The predicate to test. It must evaluate to
- *  something that can be coerced to a normal C boolean.
- *
- *  \param file A sequence of legal identifier characters that should
- *  uniquely identify the source file in which this condition appears.
- */
 
 #ifndef CASSERT_H
 #define CASSERT_H
 
+/// A compile time assertion check.
+///
+/// Validate at compile time that the predicate is true without
+/// generating code. This can be used at any point in a source file
+/// where typedef is legal.
+///
+/// On success, compilation proceeds normally.
+///
+/// On failure, attempts to typedef an array type of negative size. The
+/// offending line will look like:
+///
+/// typedef _cassert_1_42{char _[2*!!(0)-1];};
+///
+/// where 1 is a unique counter, and 42 is the line number in the file on
+/// which the assertion appears.
 #define CASSERT(predicate) \
-    _CASSERT_INNER(predicate, __LINE__)
+    _CASSERT_INNER(predicate, __COUNTER__, __LINE__)
 
-#define _CASSERT_PASTE(a, b) a##b
+#define _CASSERT_PASTE(a, b, c, d) a##b##c##d
 
-#define _CASSERT_INNER(predicate, line) \
-    struct _CASSERT_PASTE(__cassert_, line){char _[2*!!(predicate)-1];}
+#define _CASSERT_INNER(predicate, counter, line) \
+    struct _CASSERT_PASTE(__cassert_, counter, _, line){char _[2*!!(predicate)-1];}
 
 #endif /* CASSERT_H */
