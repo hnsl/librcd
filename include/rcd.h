80a6ddc59f2eb04d1aef8d8a9661e25bbe3d4fdf
Hannes Landeholm <hannes@jumpstarter.io>
2015-05-10 21:42:42 +0200

    rcd.h: decl all std mem fns

--- include/rcd.h
+++ include/rcd.h
@@ -266,7 +266,12 @@ typedef unsigned long uintmax_t;
 void* alloca(size_t);
 #define alloca __builtin_alloca
 
+// Allow direct use of standard memory functions without extra includes.
 void* memcpy(void *__restrict, const void *__restrict, size_t);
+void* memmove(void *, const void *, size_t);
+void* memset(void *, int, size_t);
+int memcmp(const void *, const void *, size_t);
+void* memchr(const void *, int, size_t);
 
 /// Core stack helper adjusts the overflow_arg_area for split stacks support.
 void __morestack_adjust_overflow_arg_area(va_list va_list);
